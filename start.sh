50d0bff037b76c868cf9e69b76880ed8a0dd6ab6
kriswalpot <kriswalpot@gmail.com>
2026-01-18 10:36:37 +1100

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,9 +12,11 @@ nginx
 ffmpeg -hide_banner -loglevel warning \
   -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 \
   -i "http://icecast.vrtcdn.be/ketnetradio.aac" \
-  -c:a aac -b:a 96k -ac 2 -ar 44100 \
+  -c:a copy \
   -f hls \
   -hls_time 4 \
   -hls_list_size 8 \
   -hls_flags delete_segments+append_list \
+  -hls_segment_filename "/var/www/hls/seg_%05d.aac" \
   /var/www/hls/stream.m3u8
+
