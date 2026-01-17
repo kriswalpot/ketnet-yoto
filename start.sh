7c98f38e27113dc1814833bb8c5d5685dbd2f57d
kriswalpot <kriswalpot@gmail.com>
2026-01-18 10:41:15 +1100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,20 +3,25 @@ set -e
 
 mkdir -p /var/www/hls
 
-# Start nginx in background
-nginx
+# Start ffmpeg in a restart loop so it can't "die quietly"
+(
+  while true; do
+    echo "Starting ffmpeg -> HLS…"
+    ffmpeg -hide_banner -loglevel warning \
+      -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 \
+      -i "http://icecast.vrtcdn.be/ketnetradio.aac" \
+      -c:a copy \
+      -f hls \
+      -hls_time 4 \
+      -hls_list_size 8 \
+      -hls_flags delete_segments+append_list \
+      -hls_segment_filename "/var/www/hls/seg_%05d.aac" \
+      /var/www/hls/stream.m3u8 || true
 
-# Convert Ketnet stream to HLS
-# -hls_flags delete_segments keeps storage low
-# -hls_time small segments helps devices start quicker
-ffmpeg -hide_banner -loglevel warning \
-  -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 \
-  -i "http://icecast.vrtcdn.be/ketnetradio.aac" \
-  -c:a copy \
-  -f hls \
-  -hls_time 4 \
-  -hls_list_size 8 \
-  -hls_flags delete_segments+append_list \
-  -hls_segment_filename "/var/www/hls/seg_%05d.aac" \
-  /var/www/hls/stream.m3u8
+    echo "ffmpeg exited; restarting in 2s…"
+    sleep 2
+  done
+) &
 
+# Run nginx in the foreground (Render expects one foreground process)
+nginx -g 'daemon off;'
